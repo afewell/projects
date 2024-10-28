856c7d550e52705e6940f459252ad9ecaa77ae08
afewell <afewell@gmail.com>
2024-10-27 23:19:36 -0700

    moving dev_vm's to the new AI_DEVELOPER_ENVIRONMENT repo

--- dev_vms/scripts/modules/install_vscode_extensions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install extensions
-echo "Installing VSCode extensions..."
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-vscode-remote.remote-containers
-echo "VSCode and extensions installed successfully."
